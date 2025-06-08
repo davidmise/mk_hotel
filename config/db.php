2f98640029041d62b61963e4df985e9886051925
david <davidkingmushi@gmail.com>
2025-06-08 13:36:22 +0200

    modified the hioerarchy

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$host = 'localhost';
+$user = 'hotel_user';
+$pass = 'P@$$w0rd';
+$db   = 'mk_demo';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
